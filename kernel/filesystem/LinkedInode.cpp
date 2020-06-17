c7c80713f5e376392d05c3cbb51b107da8e1fb64
byteduck <10217842+byteduck@users.noreply.github.com>
2020-06-16 23:34:07 -0600

    Fix per-file license disclaimer copyright year

--- kernel/filesystem/LinkedInode.cpp
+++ kernel/filesystem/LinkedInode.cpp
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with duckOS.  If not, see <https://www.gnu.org/licenses/>.
 
-    Copyright (c) Byteduck 2016-$YEAR. All rights reserved.
+    Copyright (c) Byteduck 2016-2020. All rights reserved.
 */
 
 #include <kernel/kstdio.h>
