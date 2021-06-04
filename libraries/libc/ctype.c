64acbdd8c371070eac9bcf17db4cc262f09dec57
Aaron <10217842+byteduck@users.noreply.github.com>
2021-06-03 20:02:28 -0700

    libc: Fix ctype.h for C++

--- libraries/libc/ctype.c
+++ libraries/libc/ctype.c
@@ -37,3 +37,73 @@ const char _ctype_[256] = {
 		_L, _L, _L, _L, _L, _L, _L, _L,
 		_L, _L, _L, _P, _P, _P, _P, _C
 };
+
+#undef isalnum
+int isalnum(int c) {
+	return __isalnum(c);
+}
+
+#undef isalpha
+int isalpha(int c) {
+	return __isalpha(c);
+}
+
+#undef isblank
+int isblank(int c) {
+	return __isblank(c);
+}
+
+#undef iscntrl
+int iscntrl(int c) {
+	return __iscntrl(c);
+}
+
+#undef isdigit
+int isdigit(int c) {
+	return __isdigit(c);
+}
+
+#undef isgraph
+int isgraph(int c) {
+	return __isgraph(c);
+}
+
+#undef islower
+int islower(int c) {
+	return __islower(c);
+}
+
+#undef isprint
+int isprint(int c) {
+	return __isprint(c);
+}
+
+#undef ispunct
+int ispunct(int c) {
+	return __ispunct(c);
+}
+
+#undef isspace
+int isspace(int c) {
+	return __isspace(c);
+}
+
+#undef isupper
+int isupper(int c) {
+	return __isupper(c);
+}
+
+#undef isxdigit
+int isxdigit(int c) {
+	return __isxdigit(c);
+}
+
+#undef tolower
+int tolower(int c) {
+	return __tolower(c);
+}
+
+#undef toupper
+int toupper(int c) {
+	return __toupper(c);
+}
