b45b8590489dc34063667632e4fe6ad594e39a0e
Aaron <10217842+byteduck@users.noreply.github.com>
2021-08-27 23:37:56 -0600

    libc: Fix strncpy implementation

--- libraries/libc/string.c
+++ libraries/libc/string.c
@@ -58,7 +58,7 @@ char* strncpy(char* dest, const char* src, size_t n) {
 	size_t i = 0;
 	for(; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	if(i < n)
+	if(i <= n)
 		dest[i] = '\0';
 	return dest;
 }
