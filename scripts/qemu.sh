e3685b37fa6ca5ec160a5d8305c688c821863965
Aaron <10217842+byteduck@users.noreply.github.com>
2021-10-19 15:10:30 -0700

    QEMU script with support for WSL

---
+++ scripts/qemu.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+# Check for KVM Support
+if [ -z "$USE_KVM" ]; then
+	USE_KVM="0"
+	if [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ]; then
+		USE_KVM="1"
+	fi
+fi
+
+if [ -z "$DUCKOS_IMAGE" ]; then
+	DUCKOS_IMAGE="duckOS.img"
+fi
+
+# Determine what acceleration we should use
+if [ -z "$DUCKOS_QEMU_ACCEL" ]; then
+	if command -v wslpath >/dev/null; then
+		DUCKOS_QEMU_ACCEL="-accel whpx,kernel-irqchip=off -accel tcg"
+	else
+		if [ "$USE_KVM" -ne "0" ]; then
+			DUCKOS_QEMU_ACCEL="-use-kvm"
+		else
+			DUCKOS_QEMU_ACCEL=""
+		fi
+	fi
+fi
+
+# Find which qemu binary we should use
+if command -v wslpath >/dev/null; then
+	# We're on windows, use windows QEMU
+	if [ -z "$DUCKOS_WIN_QEMU_INSTALL_DIR" ]; then
+		DUCKOS_WIN_QEMU_INSTALL_DIR="C:\\Program Files\\qemu"
+	fi
+	USE_KVM="0"
+	DUCKOS_QEMU="$(wslpath "${DUCKOS_WIN_QEMU_INSTALL_DIR}")/qemu-system-x86_64.exe"
+	DUCKOS_IMAGE="$(wslpath -w "$DUCKOS_IMAGE")"
+else
+	DUCKOS_QEMU="qemu-system-x86_64"
+fi
+
+# Run!
+DUCKOS_QEMU_ARGS="
+	-s
+	-kernel kernel/duckk32
+	-append \"\"
+	-drive file=$DUCKOS_IMAGE,cache=directsync,format=raw,id=disk,if=ide
+	-m 512M
+	-serial stdio
+	-display sdl
+	$DUCKOS_QEMU_ACCEL"
+
+"$DUCKOS_QEMU" $DUCKOS_QEMU_ARGS
