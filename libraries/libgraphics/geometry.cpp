20be97695209e1a6a30684baf1e3af3b48fa6dd1
Aaron <10217842+byteduck@users.noreply.github.com>
2021-11-23 13:24:05 -0800

    libgraphics: inline lots of geometry functions

--- libraries/libgraphics/geometry.cpp
+++ libraries/libgraphics/geometry.cpp
@@ -17,15 +17,9 @@
     Copyright (c) Byteduck 2016-2020. All rights reserved.
 */
 
-#include <algorithm>
 #include "geometry.h"
 
-bool Point::in(const Rect& rect) const {
-	return x >= rect.x && x <= rect.x + rect.width &&
-		   y >= rect.y && y <= rect.y + rect.height;
-}
-
-bool Point::near_border(const Rect &rect, int border_size) const {
+bool Point::near_border(const Rect& rect, int border_size) const {
 	Rect border_rect = {
 			rect.x - border_size,
 			rect.y - border_size,
@@ -44,167 +38,14 @@ bool Point::near_border(const Rect &rect, int border_size) const {
 }
 
 Point Point::constrain(const Rect& rect) const {
-	Point ret = {x, y};
-	if(ret.x < rect.x)
-		ret.x = rect.x;
-	if(ret.x >= rect.x + rect.width)
-		ret.x = rect.x + rect.width - (rect.width ? 1 : 0);
-	if(ret.y < rect.y)
-		ret.y = rect.y;
-	if(ret.y >= rect.y + rect.height && rect.height)
-		ret.y = rect.y + rect.height - (rect.height ? 1 : 0);
-	return ret;
-}
-
-Point Point::operator+(const Point& other) const {
-	return {x + other.x , y + other.y};
-}
-
-Point Point::operator-(const Point& other) const {
-	return {x - other.x, y - other.y};
-}
-
-Point Point::operator*(const int& scalar) const {
-	return {x * scalar, y * scalar};
-}
-
-Point Point::operator/(const int& scalar) const {
-	return {x / scalar, y / scalar};
-}
-
-bool Point::operator==(const Point& other) const {
-	return other.x == x && other.y == y;
-}
-
-
-Dimensions Dimensions::operator+(const Dimensions& other) const {
-	return {width + other.width, height + other.height};
-}
-
-Dimensions Dimensions::operator-(const Dimensions& other) const {
-	return {width - other.width, height - other.height};
-}
-
-Dimensions Dimensions::operator*(int scalar) const {
-	return {width * scalar, height * scalar};
-}
-
-Dimensions Dimensions::operator/(int scalar) const {
-	return {width / scalar, height / scalar};
-}
-
-bool Dimensions::operator==(const Dimensions& other) const {
-	return height == other.height && width == other.width;
-}
-
-
-Point Rect::position() const {
-	return {x, y};
-}
-
-void Rect::set_position(Point pos) {
-	x = pos.x;
-	y = pos.y;
-}
-
-Dimensions Rect::dimensions() const {
-	return {width, height};
-}
-
-void Rect::set_dimensions(Dimensions dims) {
-	width = dims.width;
-	height = dims.height;
-}
-
-bool Rect::collides(const Rect& other) const {
-	return (x < other.x + other.width) && (x + width > other.x) &&
-		(y < other.y + other.height) && (y + height > other.y);
-}
-
-bool Rect::inside(const Rect& other) const {
-	return x >= other.x && y >= other.y && (x + width) <= (other.x + other.width) && (y + height) <= (other.y + other.height);
-}
-
-bool Rect::contains(const Rect& other) const {
-	return other.inside(*this);
-}
-
-int Rect::area() const {
-	return width * height;
-}
-
-Rect Rect::transform(const Point& point) const {
-	return {x + point.x, y + point.y, width, height};
-}
-
-Rect Rect::constrain_relative(const Rect& other) const {
-	Rect ret = {x, y, width, height};
-
-	if(ret.width > other.width)
-		ret.width = other.width;
-	if(ret.height > other.height)
-		ret.height = other.height;
-	if(ret.x < 0)
-		ret.x = 0;
-	if(ret.y < 0)
-		ret.y = 0;
-	if(ret.x + ret.width >= other.width)
-		ret.x = other.width - ret.width;
-	if(ret.y + ret.height >= other.height)
-		ret.y = other.height - ret.height;
-
-	return ret;
-}
-
-Rect Rect::constrain(const Rect& other) const {
-	Rect ret = {x, y, width, height};
-
-	if(ret.width > other.width)
-		ret.width = other.width;
-	if(ret.height > other.height)
-		ret.height = other.height;
-	if(ret.x < other.x)
-		ret.x = other.x;
-	if(ret.y < other.y)
-		ret.y = other.y;
-	if(ret.x + ret.width >= other.x + other.width)
-		ret.x = other.x + other.width - ret.width;
-	if(ret.y + ret.height >= other.y + other.height)
-		ret.y = other.y + other.height - ret.height;
-
-	return ret;
-}
-
-Rect Rect::overlapping_area(const Rect& other) const {
-	if(!collides(other))
-		return {x, y, 0, 0};
-
-	Rect ret = {std::max(x, other.x), std::max(y, other.y), width, height};
-
-	if(ret.x + ret.width > x + width)
-		ret.width = x + width - ret.x;
-	if(ret.x + ret.width > other.x + other.width)
-		ret.width = other.x + other.width - ret.x;
-	if(ret.y + ret.height > y + height)
-		ret.height = y + height - ret.y;
-	if(ret.y + ret.height > other.y + other.height)
-		ret.height = other.y + other.height - ret.y;
-
-	return ret;
-}
-
-Rect Rect::combine(const Rect &other) const {
-	Point topleft_pos =  {std::min(x, other.x), std::min(y, other.y)};
-	Point bottomright_pos = {std::max(x + width, other.x + other.width), std::max(y + height, other.y + other.height)};
-	return Rect {
-		topleft_pos.x,
-		topleft_pos.y,
-		bottomright_pos.x - topleft_pos.x,
-		bottomright_pos.y - topleft_pos.y
-	};
-}
-
-
-bool Rect::empty() const {
-	return !width && !height;
+		Point ret = {x, y};
+		if(ret.x < rect.x)
+			ret.x = rect.x;
+		if(ret.x >= rect.x + rect.width)
+			ret.x = rect.x + rect.width - (rect.width ? 1 : 0);
+		if(ret.y < rect.y)
+			ret.y = rect.y;
+		if(ret.y >= rect.y + rect.height && rect.height)
+			ret.y = rect.y + rect.height - (rect.height ? 1 : 0);
+		return ret;
 }
--- libraries/libgraphics/geometry.h
+++ libraries/libgraphics/geometry.h
@@ -20,6 +20,8 @@
 #ifndef DUCKOS_LIBGRAPHICS_GEOMETRY_H
 #define DUCKOS_LIBGRAPHICS_GEOMETRY_H
 
+#include <algorithm>
+
 class Rect;
 
 class Point {
@@ -27,15 +29,29 @@ public:
 	int x;
 	int y;
 
-	bool in(const Rect& rect) const;
+	inline bool in(const Rect& rect) const;
 	bool near_border(const Rect& rect, int border_size) const;
 	Point constrain(const Rect& rect) const;
 
-	Point operator+(const Point& other) const;
-	Point operator-(const Point& other) const;
-	Point operator*(const int& scalar) const;
-	Point operator/(const int& scalar) const;
-	bool operator==(const Point& other) const;
+	inline Point operator+(const Point& other) const {
+		return {x + other.x , y + other.y};
+	}
+
+	inline Point operator-(const Point& other) const {
+		return {x - other.x, y - other.y};
+	}
+
+	inline Point operator*(const int& scalar) const {
+		return {x * scalar, y * scalar};
+	}
+
+	inline Point operator/(const int& scalar) const {
+		return {x / scalar, y / scalar};
+	}
+
+	inline bool operator==(const Point& other) const {
+		return other.x == x && other.y == y;
+	}
 };
 
 class Dimensions {
@@ -43,11 +59,29 @@ public:
 	int width;
 	int height;
 
-	Dimensions operator+(const Dimensions& other) const;
-	Dimensions operator-(const Dimensions& other) const;
-	Dimensions operator*(int scalar) const;
-	Dimensions operator/(int scalar) const;
-	bool operator==(const Dimensions& other) const;
+	inline Dimensions operator+(const Dimensions& other) const {
+		return {width + other.width, height + other.height};
+	}
+
+	inline Dimensions operator-(const Dimensions& other) const {
+		return {width - other.width, height - other.height};
+	}
+
+	inline Dimensions operator*(int scalar) const {
+		return {width * scalar, height * scalar};
+	}
+
+	inline Dimensions operator/(int scalar) const {
+		return {width / scalar, height / scalar};
+	}
+
+	inline bool operator==(const Dimensions& other) const {
+		return height == other.height && width == other.width;
+	}
+
+	inline bool operator!=(const Dimensions& other) const {
+		return !operator==(other);
+	}
 };
 
 class Rect {
@@ -60,55 +94,151 @@ public:
 	Rect(int x, int y, Dimensions dims): x(x), y(y), width(dims.width), height(dims.height) {}
 	Rect(Point pos, int width, int height): x(pos.x), y(pos.y), width(width), height(height) {}
 
-	Point position() const;
-	void set_position(Point pos);
-	Dimensions dimensions() const;
-	void set_dimensions(Dimensions dims);
-	bool collides(const Rect& other) const;
-	bool inside(const Rect& other) const;
-	bool contains(const Rect& other) const;
-	int area() const;
+	inline Point position() const {
+		return {x, y};
+	}
+
+	inline void set_position(Point pos) {
+		x = pos.x;
+		y = pos.y;
+	}
+
+	inline Dimensions dimensions() const {
+		return {width, height};
+	}
+
+	inline void set_dimensions(Dimensions dims) {
+		width = dims.width;
+		height = dims.height;
+	}
+
+	inline bool collides(const Rect& other) const {
+		return (x < other.x + other.width) && (x + width > other.x) &&
+		(y < other.y + other.height) && (y + height > other.y);
+	}
+
+	inline bool inside(const Rect& other) const {
+		return x >= other.x && y >= other.y
+			&& (x + width) <= (other.x + other.width)
+			&& (y + height) <= (other.y + other.height);
+	}
+
+	inline bool contains(const Rect& other) const {
+		return other.inside(*this);
+	}
+
+	inline int area() const {
+		return width * height;
+	}
 
 	/**
 	 * Returns a new rect with the same rect with the position transformed by point's position.
 	 * @return A new transformed rect.
 	 */
-	Rect transform(const Point& point) const;
+	inline Rect transform(const Point& point) const {
+		return {x + point.x, y + point.y, width, height};
+	}
 
 	/**
 	 * Returns a new rect resized and moved to fit inside the parent with relative coordinates
 	 * ((0,0) being the top left of the parent rect)
 	 *
-	 * @param parent The parent rect to constrain this rect to.
+	 * @param other The parent rect to constrain this rect to.
 	 * @return The new, possibly resized and/or moved rect
 	 */
-	Rect constrain_relative(const Rect& parent) const;
+	Rect constrain_relative(const Rect& other) const {
+		Rect ret = {x, y, width, height};
+
+		if(ret.width > other.width)
+			ret.width = other.width;
+		if(ret.height > other.height)
+			ret.height = other.height;
+		if(ret.x < 0)
+			ret.x = 0;
+		if(ret.y < 0)
+			ret.y = 0;
+		if(ret.x + ret.width >= other.width)
+			ret.x = other.width - ret.width;
+		if(ret.y + ret.height >= other.height)
+			ret.y = other.height - ret.height;
+
+		return ret;
+	}
 
 	/**
 	 * Returns a new rect resized and moved to fit inside the parent with absolute coordinates
-	 * @param parent The parent rect to constrain this rect to.
+	 * @param other The parent rect to constrain this rect to.
 	 * @return The new, possibly resized and/or moved rect
 	 */
-	Rect constrain(const Rect& parent) const;
+	Rect constrain(const Rect& other) const {
+		Rect ret = {x, y, width, height};
+
+		if(ret.width > other.width)
+			ret.width = other.width;
+		if(ret.height > other.height)
+			ret.height = other.height;
+		if(ret.x < other.x)
+			ret.x = other.x;
+		if(ret.y < other.y)
+			ret.y = other.y;
+		if(ret.x + ret.width >= other.x + other.width)
+			ret.x = other.x + other.width - ret.width;
+		if(ret.y + ret.height >= other.y + other.height)
+			ret.y = other.y + other.height - ret.height;
+
+		return ret;
+	}
 
 	/**
 	 * Returns a rect consisting of the overlapping area of two rectangles.
 	 * @param other The rect to use.
 	 * @return The overlapping area of the two rectangles.
 	 */
-	Rect overlapping_area(const Rect& other) const;
+	Rect overlapping_area(const Rect& other) const {
+		if(!collides(other))
+			return {x, y, 0, 0};
+
+		Rect ret = {std::max(x, other.x), std::max(y, other.y), width, height};
+
+		if(ret.x + ret.width > x + width)
+			ret.width = x + width - ret.x;
+		if(ret.x + ret.width > other.x + other.width)
+			ret.width = other.x + other.width - ret.x;
+		if(ret.y + ret.height > y + height)
+			ret.height = y + height - ret.y;
+		if(ret.y + ret.height > other.y + other.height)
+			ret.height = other.y + other.height - ret.y;
+
+		return ret;
+	}
 
 	/**
 	 * Returns a rect that would contain both rectangles.
 	 * @param other The other rectangle to combine with.
 	 * @return The combined rect.
 	 */
-	Rect combine(const Rect& other) const;
+	Rect combine(const Rect& other) const {
+		Point topleft_pos =  {std::min(x, other.x), std::min(y, other.y)};
+		Point bottomright_pos = {std::max(x + width, other.x + other.width), std::max(y + height, other.y + other.height)};
+		return {
+			topleft_pos.x,
+			topleft_pos.y,
+			bottomright_pos.x - topleft_pos.x,
+			bottomright_pos.y - topleft_pos.y
+		};
+	}
 
 	/**
 	 * Returns true if the rect has an area of zero.
 	 */
-	bool empty() const;
+	inline bool empty() const {
+		return !width && !height;
+	}
 };
+
+inline bool Point::in(const Rect& rect) const {
+	return x >= rect.x && x <= rect.x + rect.width &&
+		y >= rect.y && y <= rect.y + rect.height;
+}
 
 #endif //DUCKOS_LIBGRAPHICS_GEOMETRY_H
