b3dff60973a21a86a4751dda035a467b494c408c
Aaron <10217842+byteduck@users.noreply.github.com>
2021-12-08 13:32:46 -0800

    Update and reformat file headers

--- libraries/libc/poll.h
+++ libraries/libc/poll.h
@@ -1,20 +1,20 @@
 /*
-    This file is part of duckOS.
+	This file is part of duckOS.
 
-    duckOS is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	duckOS is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    duckOS is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	duckOS is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with duckOS.  If not, see <https://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with duckOS.  If not, see <https://www.gnu.org/licenses/>.
 
-    Copyright (c) Byteduck 2016-2020. All rights reserved.
+	Copyright (c) Byteduck 2016-2021. All rights reserved.
 */
 
 #ifndef DUCKOS_LIBC_POLL_H
