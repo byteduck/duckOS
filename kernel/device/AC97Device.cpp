569677352ad5b0ead3be96de9d3309de71e1c419
Aaron <10217842+byteduck@users.noreply.github.com>
2022-02-04 15:14:49 -0800

    Kernel: Replace spaces with tabs in AC97Device

--- kernel/device/AC97Device.cpp
+++ kernel/device/AC97Device.cpp
@@ -41,38 +41,38 @@ ResultRet<kstd::shared_ptr<AC97Device>> AC97Device::detect() {
 }
 
 AC97Device::AC97Device(PCI::Address address):
-    CharacterDevice(69, 2),
-    IRQHandler(PCI::read_byte(address, PCI_INTERRUPT_LINE)),
+	CharacterDevice(69, 2),
+	IRQHandler(PCI::read_byte(address, PCI_INTERRUPT_LINE)),
 	m_address(address),
 	m_mixer_address(PCI::read_word(address, PCI_BAR0) & ~1),
-    m_bus_address(PCI::read_word(address, PCI_BAR1) & ~1),
-    m_output_channel(m_bus_address + BusRegisters::NABM_PCM_OUT),
-    m_output_buffer_region(PageDirectory::k_alloc_region(PAGE_SIZE * AC97_OUTPUT_BUFFER_PAGES)),
-    m_output_buffer_descriptor_region(PageDirectory::k_alloc_region(sizeof(BufferDescriptor) * AC97_NUM_BUFFER_DESCRIPTORS)),
-    m_output_buffer_descriptors((BufferDescriptor*) m_output_buffer_descriptor_region.virt->start)
+	m_bus_address(PCI::read_word(address, PCI_BAR1) & ~1),
+	m_output_channel(m_bus_address + BusRegisters::NABM_PCM_OUT),
+	m_output_buffer_region(PageDirectory::k_alloc_region(PAGE_SIZE * AC97_OUTPUT_BUFFER_PAGES)),
+	m_output_buffer_descriptor_region(PageDirectory::k_alloc_region(sizeof(BufferDescriptor) * AC97_NUM_BUFFER_DESCRIPTORS)),
+	m_output_buffer_descriptors((BufferDescriptor*) m_output_buffer_descriptor_region.virt->start)
 {
-    //Enable bus mastering and interrupts
-    PCI::enable_interrupt(m_address);
-    PCI::enable_bus_mastering(m_address);
+	//Enable bus mastering and interrupts
+	PCI::enable_interrupt(m_address);
+	PCI::enable_bus_mastering(m_address);
 
-    //Initialize the card with cold reset of bus and mixer, enable interrupts
-    auto control = IO::inb(m_bus_address + BusRegisters::GLOBAL_CONTROL);
-    control |= GlobalControl::COLD_RESET | GlobalControl::INTERRUPT_ENABLE;
-    IO::outb(m_bus_address + BusRegisters::GLOBAL_CONTROL, control);
-    write_mixer(RESET, 1);
+	//Initialize the card with cold reset of bus and mixer, enable interrupts
+	auto control = IO::inb(m_bus_address + BusRegisters::GLOBAL_CONTROL);
+	control |= GlobalControl::COLD_RESET | GlobalControl::INTERRUPT_ENABLE;
+	IO::outb(m_bus_address + BusRegisters::GLOBAL_CONTROL, control);
+	write_mixer(RESET, 1);
 
-    //TODO: Verify version?
+	//TODO: Verify version?
 
-    //Set master volume and reset pcm out channel
-    write_mixer(MASTER_VOLUME, 0);
-    write_mixer(PCM_VOLUME, 0);
-    set_sample_rate(48000);
-    reset_output();
+	//Set master volume and reset pcm out channel
+	write_mixer(MASTER_VOLUME, 0);
+	write_mixer(PCM_VOLUME, 0);
+	set_sample_rate(48000);
+	reset_output();
 }
 
 AC97Device::~AC97Device() {
-    PageDirectory::k_free_region(m_output_buffer_region);
-    PageDirectory::k_free_region(m_output_buffer_descriptor_region);
+	PageDirectory::k_free_region(m_output_buffer_region);
+	PageDirectory::k_free_region(m_output_buffer_descriptor_region);
 }
 
 ssize_t AC97Device::read(FileDescriptor& fd, size_t offset, uint8_t* buffer, size_t count) {
@@ -80,104 +80,104 @@ ssize_t AC97Device::read(FileDescriptor& fd, size_t offset, uint8_t* buffer, siz
 }
 
 ssize_t AC97Device::write(FileDescriptor& fd, size_t, const uint8_t* buffer, size_t count) {
-    //Write buffer by buffer
-    size_t n_written = 0;
-    while(count) {
-        //Wait until we have a free buffer to write to
-        asm volatile("cli");
-        do {
-            //Read the status, current index, and last valid index
-            auto status_byte = IO::inw(m_output_channel + ChannelRegisters::STATUS);
-            BufferStatus status = {.value = status_byte};
-            auto current_index = IO::inb(m_output_channel + ChannelRegisters::CURRENT_INDEX);
-            auto last_valid_index = IO::inb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX);
-            auto num_buffers_left = last_valid_index >= current_index ? last_valid_index - current_index : AC97_NUM_BUFFER_DESCRIPTORS - (current_index - last_valid_index);
-            if(!status.is_halted)
-                num_buffers_left++;
-            if(num_buffers_left < AC97_NUM_BUFFER_DESCRIPTORS)
-                break;
-            m_blocker.set_ready(false);
-            TaskManager::current_thread()->block(m_blocker);
-        } while(m_output_dma_enabled);
-        asm volatile("sti");
+	//Write buffer by buffer
+	size_t n_written = 0;
+	while(count) {
+		//Wait until we have a free buffer to write to
+		asm volatile("cli");
+		do {
+			//Read the status, current index, and last valid index
+			auto status_byte = IO::inw(m_output_channel + ChannelRegisters::STATUS);
+			BufferStatus status = {.value = status_byte};
+			auto current_index = IO::inb(m_output_channel + ChannelRegisters::CURRENT_INDEX);
+			auto last_valid_index = IO::inb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX);
+			auto num_buffers_left = last_valid_index >= current_index ? last_valid_index - current_index : AC97_NUM_BUFFER_DESCRIPTORS - (current_index - last_valid_index);
+			if(!status.is_halted)
+				num_buffers_left++;
+			if(num_buffers_left < AC97_NUM_BUFFER_DESCRIPTORS)
+				break;
+			m_blocker.set_ready(false);
+			TaskManager::current_thread()->block(m_blocker);
+		} while(m_output_dma_enabled);
+		asm volatile("sti");
 
-        //If the output DMA is not currently enabled, reset the PCM channel to be sure
-        if(!m_output_dma_enabled)
-            reset_output();
+		//If the output DMA is not currently enabled, reset the PCM channel to be sure
+		if(!m_output_dma_enabled)
+			reset_output();
 
-        //Copy as much data as is applicable to the current output buffer
-        auto* output_buffer = (uint32_t*)(m_output_buffer_region.virt->start + PAGE_SIZE * m_current_output_buffer_page);
-        size_t num_bytes = min(count, PAGE_SIZE);
-        memcpy(output_buffer, buffer, num_bytes);
-        buffer += num_bytes;
-        count -= num_bytes;
-        n_written += num_bytes;
+		//Copy as much data as is applicable to the current output buffer
+		auto* output_buffer = (uint32_t*)(m_output_buffer_region.virt->start + PAGE_SIZE * m_current_output_buffer_page);
+		size_t num_bytes = min(count, PAGE_SIZE);
+		memcpy(output_buffer, buffer, num_bytes);
+		buffer += num_bytes;
+		count -= num_bytes;
+		n_written += num_bytes;
 
-        //Create the buffer descriptor
-        auto* descriptor = &m_output_buffer_descriptors[m_current_buffer_descriptor];
-        descriptor->data_addr = m_output_buffer_region.phys->start + PAGE_SIZE * m_current_output_buffer_page;
-        descriptor->num_samples = num_bytes / sizeof(uint16_t);
-        descriptor->flags = {false, true};
+		//Create the buffer descriptor
+		auto* descriptor = &m_output_buffer_descriptors[m_current_buffer_descriptor];
+		descriptor->data_addr = m_output_buffer_region.phys->start + PAGE_SIZE * m_current_output_buffer_page;
+		descriptor->num_samples = num_bytes / sizeof(uint16_t);
+		descriptor->flags = {false, true};
 
-        //Set the buffer descriptor list address and last valid index in the channel registers
-        IO::outl(m_output_channel + ChannelRegisters::BUFFER_LIST_ADDR, m_output_buffer_descriptor_region.phys->start);
-        IO::outb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX, m_current_buffer_descriptor);
+		//Set the buffer descriptor list address and last valid index in the channel registers
+		IO::outl(m_output_channel + ChannelRegisters::BUFFER_LIST_ADDR, m_output_buffer_descriptor_region.phys->start);
+		IO::outb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX, m_current_buffer_descriptor);
 
-        //If the output DMA is not enabled already, enable it
-        if(!m_output_dma_enabled) {
-            auto ctrl = IO::inb(m_output_channel + ChannelRegisters::CONTROL);
-            ctrl |= ControlFlags::PAUSE_BUS_MASTER | ControlFlags::ERROR_INTERRUPT | ControlFlags::COMPLETION_INTERRUPT;
-            IO::outb(m_output_channel + ChannelRegisters::CONTROL, ctrl);
-            m_output_dma_enabled = true;
-        }
+		//If the output DMA is not enabled already, enable it
+		if(!m_output_dma_enabled) {
+			auto ctrl = IO::inb(m_output_channel + ChannelRegisters::CONTROL);
+			ctrl |= ControlFlags::PAUSE_BUS_MASTER | ControlFlags::ERROR_INTERRUPT | ControlFlags::COMPLETION_INTERRUPT;
+			IO::outb(m_output_channel + ChannelRegisters::CONTROL, ctrl);
+			m_output_dma_enabled = true;
+		}
 
-        //Increment buffer page and buffer descriptor index
-        m_current_output_buffer_page++;
-        m_current_output_buffer_page %= AC97_OUTPUT_BUFFER_PAGES;
-        m_current_buffer_descriptor++;
-        m_current_buffer_descriptor %= AC97_NUM_BUFFER_DESCRIPTORS;
-    }
+		//Increment buffer page and buffer descriptor index
+		m_current_output_buffer_page++;
+		m_current_output_buffer_page %= AC97_OUTPUT_BUFFER_PAGES;
+		m_current_buffer_descriptor++;
+		m_current_buffer_descriptor %= AC97_NUM_BUFFER_DESCRIPTORS;
+	}
 
-    return n_written;
+	return n_written;
 }
 
 void AC97Device::handle_irq(Registers *regs) {
-    //Read the status
-    auto status_byte = IO::inw(m_output_channel + ChannelRegisters::STATUS);
-    BufferStatus status = {.value = status_byte};
+	//Read the status
+	auto status_byte = IO::inw(m_output_channel + ChannelRegisters::STATUS);
+	BufferStatus status = {.value = status_byte};
 
-    if(status.fifo_error)
-        KLog::err("AC97", "Encountered FIFO error!");
+	if(status.fifo_error)
+		KLog::err("AC97", "Encountered FIFO error!");
 
-    //If we're not done, don't do anything
-    if(!status.completion_interrupt_status)
-        return;
+	//If we're not done, don't do anything
+	if(!status.completion_interrupt_status)
+		return;
 
-    status.value = 0;
-    status.completion_interrupt_status = true;
-    status.last_valid_interrupt = true;
-    status.fifo_error = true;
-    IO::outw(m_output_channel + ChannelRegisters::STATUS, status.value);
+	status.value = 0;
+	status.completion_interrupt_status = true;
+	status.last_valid_interrupt = true;
+	status.fifo_error = true;
+	IO::outw(m_output_channel + ChannelRegisters::STATUS, status.value);
 
-    auto current_index = IO::inb(m_output_channel + ChannelRegisters::CURRENT_INDEX);
-    auto last_valid_index = IO::inb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX);
-    if(last_valid_index == current_index) {
-        reset_output();
-    }
-    m_blocker.set_ready(true);
+	auto current_index = IO::inb(m_output_channel + ChannelRegisters::CURRENT_INDEX);
+	auto last_valid_index = IO::inb(m_output_channel + ChannelRegisters::LAST_VALID_INDEX);
+	if(last_valid_index == current_index) {
+		reset_output();
+	}
+	m_blocker.set_ready(true);
 }
 
 //Channel
 void AC97Device::reset_output() {
-    //Send reset command and wait for it to be cleared
-    IO::outb(m_output_channel + ChannelRegisters::CONTROL, ControlFlags::RESET_REGISTERS);
-    while(IO::inb(m_output_channel + ChannelRegisters::CONTROL) & ControlFlags::RESET_REGISTERS)
-        IO::wait(50);
-    m_output_dma_enabled = false;
-    m_current_buffer_descriptor = 0;
+	//Send reset command and wait for it to be cleared
+	IO::outb(m_output_channel + ChannelRegisters::CONTROL, ControlFlags::RESET_REGISTERS);
+	while(IO::inb(m_output_channel + ChannelRegisters::CONTROL) & ControlFlags::RESET_REGISTERS)
+		IO::wait(50);
+	m_output_dma_enabled = false;
+	m_current_buffer_descriptor = 0;
 }
 
 void AC97Device::set_sample_rate(uint32_t sample_rate) {
-    IO::outw(m_mixer_address + MixerRegisters::SAMPLE_RATE, sample_rate);
-    m_sample_rate = IO::inw(m_mixer_address + MixerRegisters::SAMPLE_RATE);
+	IO::outw(m_mixer_address + MixerRegisters::SAMPLE_RATE, sample_rate);
+	m_sample_rate = IO::inw(m_mixer_address + MixerRegisters::SAMPLE_RATE);
 }
--- kernel/device/AC97Device.h
+++ kernel/device/AC97Device.h
@@ -32,99 +32,99 @@
 
 class AC97Device: public CharacterDevice, public IRQHandler {
 public:
-    enum BusRegisters {
-        NABM_PCM_IN    = 0x00u,
-        NABM_PCM_OUT   = 0x10u,
-        NABM_MIC       = 0x20u,
-        GLOBAL_CONTROL = 0x2Cu,
-        GLOBAL_STATUS  = 0x30u
-    };
+	enum BusRegisters {
+		NABM_PCM_IN	= 0x00u,
+		NABM_PCM_OUT   = 0x10u,
+		NABM_MIC	   = 0x20u,
+		GLOBAL_CONTROL = 0x2Cu,
+		GLOBAL_STATUS  = 0x30u
+	};
 
-    enum MixerRegisters {
-        RESET         = 0x00u, //word
-        MASTER_VOLUME = 0x02u, //word
-        MIC_VOLUME    = 0x0Eu, //word
-        PCM_VOLUME    = 0x18u, //word
-        INPUT_DEVICE  = 0x1Au, //word
-        INPUT_GAIN    = 0x1Cu, //word
-        MIC_GAIN      = 0x1Eu, //word
-        SAMPLE_RATE   = 0x2Cu //word
-    };
+	enum MixerRegisters {
+		RESET		 = 0x00u, //word
+		MASTER_VOLUME = 0x02u, //word
+		MIC_VOLUME	= 0x0Eu, //word
+		PCM_VOLUME	= 0x18u, //word
+		INPUT_DEVICE  = 0x1Au, //word
+		INPUT_GAIN	= 0x1Cu, //word
+		MIC_GAIN	  = 0x1Eu, //word
+		SAMPLE_RATE   = 0x2Cu //word
+	};
 
 	enum GlobalControl {
 		INTERRUPT_ENABLE = 0b00000001,
-		COLD_RESET       = 0b00000010
+		COLD_RESET	   = 0b00000010
 	};
 
-    enum ChannelRegisters {
-        BUFFER_LIST_ADDR  = 0x00u, //dword
-        CURRENT_INDEX     = 0x04u, //byte
-        LAST_VALID_INDEX  = 0x05u, //byte
-        STATUS            = 0x06u, //word
-        BUFFER_POSITION   = 0x08u, //word
-        PREFETCHED_INDEX  = 0x0Au, //byte
-        CONTROL           = 0x0Bu, //byte
-    };
+	enum ChannelRegisters {
+		BUFFER_LIST_ADDR  = 0x00u, //dword
+		CURRENT_INDEX	 = 0x04u, //byte
+		LAST_VALID_INDEX  = 0x05u, //byte
+		STATUS			= 0x06u, //word
+		BUFFER_POSITION   = 0x08u, //word
+		PREFETCHED_INDEX  = 0x0Au, //byte
+		CONTROL		   = 0x0Bu, //byte
+	};
 
-    enum ControlFlags {
-        PAUSE_BUS_MASTER     = 0b00000001,
-        RESET_REGISTERS      = 0b00000010,
-        ERROR_INTERRUPT      = 0b00001000,
-        COMPLETION_INTERRUPT = 0b00010000
-    };
+	enum ControlFlags {
+		PAUSE_BUS_MASTER	 = 0b00000001,
+		RESET_REGISTERS	  = 0b00000010,
+		ERROR_INTERRUPT	  = 0b00001000,
+		COMPLETION_INTERRUPT = 0b00010000
+	};
 
-    struct BufferDescriptor {
-        uint32_t data_addr;
-        uint16_t num_samples;
-        struct {
-            uint16_t : 14;
-            bool is_last_entry : 1;
-            bool interrupt_on_completion : 1;
-        }  __attribute__((packed)) flags;
-    } __attribute__((packed));
+	struct BufferDescriptor {
+		uint32_t data_addr;
+		uint16_t num_samples;
+		struct {
+			uint16_t : 14;
+			bool is_last_entry : 1;
+			bool interrupt_on_completion : 1;
+		}  __attribute__((packed)) flags;
+	} __attribute__((packed));
 
-    union BufferStatus {
-        struct {
-            bool is_halted : 1;
-            bool is_last_valid : 1;
-            bool last_valid_interrupt : 1;
-            bool completion_interrupt_status : 1;
-            bool fifo_error : 1;
-            uint16_t : 11;
-        };
-        uint16_t value;
-    };
+	union BufferStatus {
+		struct {
+			bool is_halted : 1;
+			bool is_last_valid : 1;
+			bool last_valid_interrupt : 1;
+			bool completion_interrupt_status : 1;
+			bool fifo_error : 1;
+			uint16_t : 11;
+		};
+		uint16_t value;
+	};
 
-    ~AC97Device();
+	~AC97Device();
 	static ResultRet<kstd::shared_ptr<AC97Device>> detect();
 
 	//File
 	ssize_t read(FileDescriptor& fd, size_t offset, uint8_t* buffer, size_t count) override;
 	ssize_t write(FileDescriptor& fd, size_t offset, const uint8_t* buffer, size_t count) override;
 
-    //IRQHandler
-    void handle_irq(Registers* regs) override;
+	//IRQHandler
+	void handle_irq(Registers* regs) override;
 
 private:
 	explicit AC97Device(PCI::Address address);
 
-    inline void write_mixer(MixerRegisters reg, uint16_t val) const {
-        IO::outw(m_mixer_address + reg, val);
-    }
+	inline void write_mixer(MixerRegisters reg, uint16_t val) const {
+		IO::outw(m_mixer_address + reg, val);
+	}
 
-    void reset_output();
-    void set_sample_rate(uint32_t sample_rate);
+	void reset_output();
+	void set_sample_rate(uint32_t sample_rate);
 
 	PCI::Address m_address;
 	uint16_t m_mixer_address, m_bus_address, m_output_channel;
-    LinkedMemoryRegion m_output_buffer_region;
-    LinkedMemoryRegion m_output_buffer_descriptor_region;
-    BufferDescriptor* m_output_buffer_descriptors;
-    uint32_t m_current_output_buffer_page = 0;
-    uint32_t m_current_buffer_descriptor = 0;
-    bool m_output_dma_enabled = false;
-    BooleanBlocker m_blocker;
-    uint32_t m_sample_rate;
+	LinkedMemoryRegion m_output_buffer_region;
+	LinkedMemoryRegion m_output_buffer_descriptor_region;
+	BufferDescriptor* m_output_buffer_descriptors;
+	uint32_t m_current_output_buffer_page = 0;
+	uint32_t m_current_buffer_descriptor = 0;
+	bool m_output_dma_enabled = false;
+	BooleanBlocker m_blocker;
+	uint32_t m_sample_rate;
 };
 
 
