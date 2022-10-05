fe143707a948825f1da97b1c1e98edb0ee8ae8f3
Aaron <10217842+byteduck@users.noreply.github.com>
2022-10-05 01:20:19 -0700

    Kernel: Make panic message more succinct to avoid orphaned period

--- kernel/kstd/kstdio.cpp
+++ kernel/kstd/kstdio.cpp
@@ -143,7 +143,7 @@ bool panicked = false;
 	panicking = true;
 	tty->set_graphical(false);
 	printf("\033[41;97m\033[2J"); //Red BG, bright white FG
-	print("Whoops! Something terrible happened.\nIf you weren't expecting this, feel free to open an issue on GitHub to report it.\nHere are the details:\n");
+	print("Whoops! Something terrible happened.\nIf you weren't expecting this, please open an issue on GitHub to report it.\nHere are the details:\n");
 	printf("%s\n", error);
 
 	va_list list;
