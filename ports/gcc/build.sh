ff042dc3080bbaf02a836741d14e7c3d9dcfa0eb
Aaron <10217842+byteduck@users.noreply.github.com>
2022-10-10 19:57:41 -0700

    ports/gcc: Add correct build arguments

--- ports/gcc/build.sh
+++ ports/gcc/build.sh
@@ -5,4 +5,6 @@ export DOWNLOAD_URL="$GCC_URL"
 export DOWNLOAD_FILE="$GCC_FILE"
 export PATCH_FILE="gcc.patch"
 export USE_CONFIGURE="true"
-export CONFIGURE_ARGS=("--target=$TARGET" "--with-sysroot=/" "--with-build-sysroot=$ROOT_DIR" "--disable-nls" "--enable-languages=c,c++" "--with-newlib" "--enable-shared" "--enable-host-shared" "--disable-lto")
+export CONFIGURE_ARGS=("--target=$TARGET" "--with-sysroot=/" "--with-build-sysroot=$ROOT_DIR" "--disable-nls" "--enable-languages=c,c++" "--with-newlib" "--enable-shared" "--disable-lto")
+export MAKE_ARGS=("all-gcc" "all-target-libgcc" "all-target-libstdc++-v3")
+export INSTALL_ARGS=("install-gcc" "install-target-libgcc" "install-target-libstdc++-v3")
