57cc1b90e93ffba2f8a5768547e0e4f550911ea8
Aaron <10217842+byteduck@users.noreply.github.com>
2022-10-13 20:33:17 -0700

    libduck: Make SpinLock not recursive

--- libraries/libduck/SpinLock.cpp
+++ libraries/libduck/SpinLock.cpp
@@ -21,21 +21,11 @@
 #include <sys/thread.h>
 
 void Duck::SpinLock::acquire() {
-	tid_t self = gettid();
-	if(holding_thread == self) {
-		times_locked.fetch_add(1, std::memory_order_acquire);
-		return;
-	}
 	while(times_locked.exchange(1, std::memory_order_acquire));
-	holding_thread.store(self, std::memory_order_acquire);
 }
 
 void Duck::SpinLock::release() {
-	tid_t self = gettid();
-	if(holding_thread != self)
-		return;
-	if(times_locked.fetch_sub(1, std::memory_order_release) == 1)
-		holding_thread.compare_exchange_strong(self, -1, std::memory_order_relaxed, std::memory_order_relaxed);
+	times_locked.store(0, std::memory_order_release);
 }
 
 Duck::ScopedLock::ScopedLock(Duck::SpinLock& lock): lock(lock) {
