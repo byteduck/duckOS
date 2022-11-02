6cebf5b5d7b682f49bc1f9cd061b608bfd2bc078
Aaron <10217842+byteduck@users.noreply.github.com>
2022-11-02 01:17:02 -0700

    libui: New Cell widget for easy padding & background

---
+++ libraries/libui/widget/Cell.cpp
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2016-2022 Byteduck */
+
+#include "Cell.h"
+using namespace UI;
+
+const int Cell::default_padding = 4;
+const Color Cell::default_background = 0;
+const Cell::Style Cell::default_style = Cell::Style::PLAIN;
+
+Gfx::Dimensions Cell::preferred_size() {
+	return children[0]->preferred_size() + Gfx::Dimensions { m_padding * 2, m_padding * 2 };
+}
+
+Gfx::Dimensions Cell::minimum_size() {
+	return children[0]->minimum_size() + Gfx::Dimensions { m_padding * 2, m_padding * 2 };
+}
+
+void Cell::calculate_layout() {
+	children[0]->set_layout_bounds(Gfx::Rect {{0, 0}, current_size()}.inset(m_padding));
+}
+
+void Cell::do_repaint(const UI::DrawContext& ctx) {
+	switch(m_style) {
+	case Style::PLAIN:
+		ctx.fill(ctx.rect(), m_background);
+		break;
+	case Style::INSET:
+		ctx.draw_inset_rect(ctx.rect(), m_background);
+		break;
+	case Style::OUTSET:
+		ctx.draw_outset_rect(ctx.rect(), m_background);
+		break;
+	}
+}
+
+Cell::Cell(Duck::Ptr<UI::Widget> child, int padding, Color background, Style style):
+	m_padding(padding), m_background(background), m_style(style)
+{
+	add_child(child);
+	set_uses_alpha(true);
+}
---
+++ libraries/libui/widget/Cell.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2016-2022 Byteduck */
+
+#pragma once
+
+#include "Widget.h"
+
+namespace UI {
+	class Cell : public UI::Widget {
+	public:
+		WIDGET_DEF(Cell);
+
+		enum class Style {
+			PLAIN, INSET, OUTSET
+		};
+
+		static const int default_padding;
+		static const Color default_background;
+		static const Style default_style;
+
+		// Widget
+		Gfx::Dimensions preferred_size() override;
+		Gfx::Dimensions minimum_size() override;
+		void calculate_layout() override;
+		void do_repaint(const UI::DrawContext& ctx) override;
+
+	private:
+		Cell(Duck::Ptr<UI::Widget> child, int padding = default_padding, Color background = default_background, Style style = default_style);
+
+		int m_padding;
+		Color m_background;
+		Style m_style;
+	};
+}
