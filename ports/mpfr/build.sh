46e21a1eb952b7a0acd368312a0461fec7bef023
Aaron <10217842+byteduck@users.noreply.github.com>
2022-11-19 15:55:48 -0800

    Ports: Fix mpfr download URL

--- ports/mpfr/build.sh
+++ ports/mpfr/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOWNLOAD_URL="https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.xz"
+export DOWNLOAD_URL="https://ftp.gnu.org/gnu/mpfr/mpfr-4.1.0.tar.xz"
 export DOWNLOAD_FILE="mpfr-4.1.0"
 export PATCH_FILE="mpfr.patch"
 export USE_CONFIGURE="true"
