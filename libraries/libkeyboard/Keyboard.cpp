69873c99e705e22fc01edf151cc106896ea50a5e
Aaron <10217842+byteduck@users.noreply.github.com>
2023-01-12 22:05:39 -0800

    libkeyboard: Add header with some key codes

---
+++ libraries/libkeyboard/Keyboard.cpp
@@ -0,0 +1,4 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2016-2023 Byteduck */
+
+#include "Keyboard.h"
---
+++ libraries/libkeyboard/Keyboard.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/* Copyright © 2016-2023 Byteduck */
+
+#pragma once
+
+namespace Keyboard {
+	enum Key {
+		Up = 0x48,
+		Left = 0x4b,
+		Right = 0x4d,
+		Down = 0x50
+	};
+}
