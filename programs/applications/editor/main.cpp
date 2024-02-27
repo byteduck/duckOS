5cf31ab5b601c4611ddcb880a3e17f1c5ca77d88
Aaron <10217842+byteduck@users.noreply.github.com>
2024-02-27 00:05:49 -0800

    Editor: Add "Open" menu item, open to blank view by default

--- programs/applications/editor/main.cpp
+++ programs/applications/editor/main.cpp
@@ -12,31 +12,9 @@ int main(int argc, char** argv, char** envp) {
 	UI::init(argv, envp);
 
 	Duck::Path file_path = "/";
-	if(argc < 2) {
-		auto files = UI::FilePicker::make()->pick();
-		if(files.empty())
-			return EXIT_SUCCESS;
-		file_path = files[0];
-	} else {
-		file_path = argv[1];
-	}
 
 	auto window = UI::Window::make();
-
-	std::string contents;
-	{
-		auto file = TRY(File::open(file_path, "r"));
-		FileInputStream stream(file);
-		while(true) {
-			char read;
-			if(stream.read(&read, 1) != 1 || contents.size() > (1024 * 1024))
-				break;
-			if(read)
-				contents += read;
-		}
-	}
-
-	auto text_view = UI::TextView::make(contents);
+	auto text_view = UI::TextView::make("");
 
 	auto save = [&] () {
 		auto file_res = File::open(file_path, "w+");
@@ -47,18 +25,49 @@ int main(int argc, char** argv, char** envp) {
 		file.close();
 	};
 
+	auto open = [&] (Duck::Path path) {
+		file_path = path;
+		window->set_title("Editor: " + path.basename());
+		window->set_icon(UI::app_info().icon_for_file(file_path));
+		std::string contents;
+		{
+			auto file = TRY(File::open(path, "r"));
+			FileInputStream stream(file);
+			while(true) {
+				char read;
+				if(stream.read(&read, 1) != 1 || contents.size() > (1024 * 1024))
+					break;
+				if(read)
+					contents += read;
+			}
+		}
+		text_view->set_text(std::move(contents));
+		return Result::SUCCESS;
+	};
+
+	auto open_picker = [&] () {
+		auto files = UI::FilePicker::make()->pick();
+		if (files.empty())
+			return;
+		open(files[0]);
+	};
+
 	auto menu = UI::Menu::make({
 		UI::MenuItem::make("File", nullptr, UI::Menu::make({
+			UI::MenuItem::make("Open...", open_picker),
 			UI::MenuItem::make("Save", save)
 		}))
 	});
+
 	auto menu_bar = UI::MenuBar::make(menu);
 	window->set_titlebar_accessory(menu_bar);
 
 	window->set_contents(text_view);
 	window->set_resizable(true);
-	window->set_title("Editor: " + file_path.basename());
-	window->set_icon(UI::app_info().icon_for_file(file_path));
+	window->set_title("Editor");
+
+	if(argc >= 2)
+		open(argv[1]);
 
 	auto disp_rect = Gfx::Rect {{0,0}, UI::pond_context->get_display_dimensions() - Gfx::Dimensions {32, 32}};
 	if(!Gfx::Rect {{0, 0}, window->dimensions()}.inside(disp_rect))
