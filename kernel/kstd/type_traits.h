0ba0f20b73725b2c39c2e305560caaf9689b6daf
Aaron <10217842+byteduck@users.noreply.github.com>
2024-03-27 01:01:36 -0700

    Kernel: More type traits functionality

--- kernel/kstd/type_traits.h
+++ kernel/kstd/type_traits.h
@@ -23,5 +23,24 @@ namespace kstd {
 	template< class T > struct remove_reference      {typedef T type;};
 	template< class T > struct remove_reference<T&>  {typedef T type;};
 	template< class T > struct remove_reference<T&&> {typedef T type;};
+
+	template<class T, T v>
+	struct integral_constant {
+		static constexpr T value = v;
+		using value_type = T;
+		using type = integral_constant;
+		constexpr operator value_type() const noexcept { return value; }
+		constexpr value_type operator()() const noexcept { return value; }
+	};
+
+	using true_type = integral_constant<bool, true>;
+	using false_type = integral_constant<bool, false>;
+
+	template<class T, class U>
+	struct is_same : false_type {};
+	template<class T>
+	struct is_same<T, T> : true_type {};
+	template<class T, class U>
+	inline constexpr bool is_same_v = is_same<T, U>::value;
 }
 
